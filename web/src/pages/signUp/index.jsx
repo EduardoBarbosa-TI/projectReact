a9ff968c685fc137d8e28a9a0c07500c94e198d5
eduardobarbosa-ti <eduardobb@rede.ulbra.br>
2022-10-08 16:42:42 -0300

    Lidando com React.js e iniciando as rotas de requisições com bibliotexa Axios e validação de formulario com as bibliotecas Formik e Yup

--- web/src/pages/signUp/index.jsx
+++ web/src/pages/signUp/index.jsx
@@ -1,22 +1,36 @@
 import axios from 'axios'
 import { useFormik } from 'formik'
-import * as yup from "yup"
+import * as yup from 'yup'
 
 import {Icon, Input} from '~/components'
+
+const validationSchema = yup.object().shape({
+    name: yup.string().required('Preencha seu nome'),
+    username: yup.string().required('Preencha seu nome de usuário'),
+    email: yup.string().required('Preencha seu nome e-mail'),
+    password: yup.string().required('Digite sua senha')
+});
 
 
 
 
 export const SignUp = () => {
+
     const formik = useFormik({
-        onSubmit: () => {},
+        onSubmit: (values) => {
+            console.log(values)
+        },
         initialValues: {
             name: '',
             username: '',
             email: '',
             password: ''
-        }
+        },
+        validationSchema
     })
+
+    console.log(formik.errors)
+
     return (
         <div>
             <header className=" p-4 border-b border-red-300">
@@ -33,35 +47,46 @@ export const SignUp = () => {
                     <h2 className="text-xl font-bold">Crie sua conta</h2>
                 </div>
 
-                <form className="p-4 space-y-6">
+                <form className="p-4 space-y-6" onSubmit={formik.handleSubmit}>
                     <Input
                         type="text"
                         name="name"
                         label="Seu nome"
                         placeholder="Digite seu nome"
-
+                        value = {formik.values.name}
+                        onChange = {formik.handleChange}
+                        onBlur={formik.handleBlur}
                     />
                     <Input
                         type="text"
-                        name="userName"
+                        name="username"
                         label="Seu nome de usuário"
                         placeholder="Digite um nome de usuário"
+                        value = {formik.values.username}
+                        onChange = {formik.handleChange}
+                        onBlur={formik.handleBlur}
                     />
                      <Input
                         type="email"
                         name="email"
                         label="Seu e-mail"
                         placeholder="Digite seu e-mail"
+                        value = {formik.values.email}
+                        onChange = {formik.handleChange}
+                        onBlur={formik.handleBlur}
                     />
                      <Input
                         type="password"
                         name="password"
                         label="Sua senha"
                         placeholder="Digite sua senha"
+                        value = {formik.values.password}
+                        onChange = {formik.handleChange}
+                        onBlur={formik.handleBlur}
                     />
-                    <a href='/dashboard' className="block w-full text-center text-white bg-red-500  px-6 py-3 rounded-xl">
+                    <button type='submit' className="block w-full text-center text-white bg-red-500  px-6 py-3 rounded-xl">
                         Criar minha conta 
-                    </a>
+                    </button>
                 </form>
             </main>
         </div>
